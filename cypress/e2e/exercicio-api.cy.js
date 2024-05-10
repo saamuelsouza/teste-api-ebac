d64fc4b15fef4e86df7d76218d06a84673184b8d
Samuel Souza <saamuelfelipe@gmail.com>
2024-05-10 12:07:29 -0300

    Alteração na ordem dos testes, para que ocorram como um fluxo iniciando pelo cadastro e finalizando com a exclusão para limpar do BD.

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -1,51 +1,24 @@
 /// <reference types="cypress" />
 
 describe('Testes da Funcionalidade Usuários', () => {
-  let token
-  before(() => {
-      cy.token('fulano@qa.com', 'teste').then(tkn => { token = tkn })
-  });
-  
 
-  it('Deve validar contrato de usuários', () => {
-    cy.request('usuarios').then(response => {
-      return contrato.validateAsync(response.body)
-  })
+  it('Deve cadastrar um usuário com sucesso', () => {
+    //TODO: 
   });
 
-  it('Deve listar usuários cadastrados - GET', () => {
-    cy.request({
-      method: 'GET',
-      url: 'usuarios'
-  }).then((response) => {
-      expect(response.status).to.equal(200)
-      expect(response.body).to.have.property('usuarios')
-  }) 
+  it('Deve listar usuários cadastrados', () => {
+    //TODO: 
   });
 
-  it.only('Deve cadastrar um usuário com sucesso', () => {
-    let usuario = `Usuario ${Math.floor(Math.random() * 100000000)}`
-        cy.request({
-            method: 'POST',
-            url: 'usuario',
-            body: {
-              "nome": "Fulano da Silva 1",
-              "email": "beltrano@qa.com.br",
-              "password": "teste",
-              "administrador": "true"
-            },
-            headers: { authorization: token }
-        }).then((response) => {
-            expect(response.status).to.equal(200)
-            expect(response.body.message).to.equal('Cadastro realizado com sucesso')
-        }) 
+  it('Deve editar um usuário previamente cadastrado', () => {
+    //TODO: 
   });
 
   it('Deve validar um usuário com email inválido', () => {
     //TODO: 
   });
 
-  it('Deve editar um usuário previamente cadastrado', () => {
+  it('Deve validar contrato de usuários', () => {
     //TODO: 
   });
 
